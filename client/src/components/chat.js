423f7ef70f033a4266803e235754b89dd506bc75
Chris Trinh <christrinh5@gmail.com>
2019-12-23 21:08:57 -0800

    First commit, set up application

---
+++ client/src/components/chat.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Chat = () => {
+  return (
+    <div>
+      CHAT
+    </div>
+  )
+}
+
+export default Chat
---
+++ client/src/components/join.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Join = () => {
+  return (
+    <div>
+      Join
+    </div>
+  )
+}
+
+export default Join
